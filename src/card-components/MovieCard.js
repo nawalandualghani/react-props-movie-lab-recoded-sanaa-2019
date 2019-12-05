2f7f4fb5b300b74776929806bc4e587bf7c920a5
nawal abdualghani <nawalabdualghani@gmail.com>
2019-12-05 08:33:24 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -43,4 +43,12 @@ export default class MovieCard extends Component {
   }
 }
 
+
+
 // Don't forget your default props!
+MovieCard.defaultProps = {
+	title: 'Unknown',
+	IMDBRating: null,
+	genres: ['No Genre(s) Found'],
+	poster: 'default'
+};
